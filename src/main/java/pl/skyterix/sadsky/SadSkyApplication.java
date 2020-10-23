7d0c01d6a91d9f07aa6cd74601cd352fef88fbc9
Skyterix <skyterix1991@gmail.com>
2020-10-23 11:40:44 +0200

    Application public fixed

--- src/main/java/pl/skyterix/sadsky/SadSkyApplication.java
+++ src/main/java/pl/skyterix/sadsky/SadSkyApplication.java
@@ -6,7 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-class SadSkyApplication {
+public class SadSkyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SadSkyApplication.class, args);
